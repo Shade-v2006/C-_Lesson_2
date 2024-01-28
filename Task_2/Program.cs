2ec08186146003e6019bc005201f7a52f82e707f
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-29 02:11:21 +0700

    add lesson_2:task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n]; //Создание массива с неизвестным количеством элементов
+int i = 0;
+
+while (i < arr.Length) // arr.Length - подразумевает кол-во элементов выделенных под массив (аналог i<n)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(" ");
+    Console.Write($"{arr[i]} "); // В место 8 и 9 строки можно сделать так
+    i++;
+
+}
